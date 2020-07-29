3df9580ed65e04d711126dd06600075a9714c279
Abhishek S <28806489+abs428@users.noreply.github.com>
2020-07-29 08:26:20 +0530

    Added more code documentation. There is some more clarity regarding the methodology

--- deadringer.py
+++ deadringer.py
@@ -24,7 +24,44 @@ class NosiyData:
     def perturb(probabilities: typing.Dict[str,typing.Any[str,float]]):
         '''Function that takes in a dictionary of perturbation probabilities as
         input (these values refer to the probabilities of transposition,
-        substitution, insertion etc) for each field.
+        substitution, insertion etc) for each field. The supported probabilities
+        are:-
+
+        - selection: Probability of selecting a field for introducing one or
+                     more modifications (set this to 0.0 if no modifications
+                     should be introduced into this field ever). Note: The sum
+                     of these select probabilities over all defined fields must
+                     be 1. The default for this will be computed based on the
+                     values provided for the other fields. For instance, if
+                     FIELD_A has a selection probability of 0.8 and FIELD_B has
+                     a selection probability of 0.1, then if FIELD_C has no
+                     assigned selection probability, then the default is
+                     1 - (0.8 + 0.1) = 0.1. If another field, FIELD_D, also
+                     has no selection probability, then the assigned probability,
+                     then the default is (1 - (0.8 + 0.1))/2 = 0.05, ie:- the
+                     remaining probability mass is split evenly between the two
+                     fields. TODO: Instead of avergaing, would it be possible to
+                     share based on the base rates from the literature?
+
+        - misspelling: Probability of swapping an original value with a randomly
+                       chosen misspelling from the corresponding misspelling
+                       dictionary (can only be set to larger than 0.0 if such a
+                       misspellings dictionary is defined for the given field).
+                       TODO: Decide the API for misspellings dictionary.
+
+        - insertion: Probability of inserting a character into a field value.
+        - deletion: Probability of deleting a character from a field value.
+        - substitution: Probability of substituting a character in a field value
+                        with another character.
+        - transposition: Probability of transposing two characters in a field
+                         value.
+        - swap_fields: Probability of swapping the value in a field with another
+                       (randomly selected) value for this field. TODO: Are some
+                       fields more liable to be swapped rather than others?
+                       FirstName-LastName swaps might be more probable than
+                       swapping names with addresses.
+        - space_insertion: Probability of inserting a space into a field value
+                           (thus splitting a word).
 
         Parameters
         ----------
